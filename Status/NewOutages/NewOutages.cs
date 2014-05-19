d6bcdefaac8d581ab9d105ca261118fe0989ac28
blods <robin_sayer@hotmail.com>
2014-05-19 16:44:59 -0700

    EMail working with formatting and removal of duplicates

--- Status/NewOutages/NewOutages.cs
+++ Status/NewOutages/NewOutages.cs
@@ -5,6 +5,7 @@ using Microsoft.SharePoint.Utilities;
 using Microsoft.SharePoint.Workflow;
 using System.Net.Mail;          // Added for the email
 using System.Linq;
+using System.Collections.Generic;   // Added for list
 
 
 namespace Status.NewOutages
@@ -24,6 +25,8 @@ namespace Status.NewOutages
         public string outageDetails="";
         public string outageDefcon="";
         public string outageTrackingRef="";
+
+        
 
         /// <summary>
         /// An item is being added.
@@ -46,9 +49,13 @@ namespace Status.NewOutages
 
         public void BuildEmail(SPItemEventProperties prop)
         {
+
+            List<string> recipients = new List<string>();   // To store the email recipients
+
             // Only do something if 'Activate Emails' is set to yes
             if (prop.ListItem["Activate Emails"].ToString() == "True")
             {
+                // Copy the outage items over to variables
                 if (prop.ListItem["Title"].ToString() != null) outageTitle = prop.ListItem["Title"].ToString();
                 outageSystem = prop.ListItem["System"].ToString();
                 if (prop.ListItem["Update"] != null) outageUpdate = prop.ListItem["Update"].ToString();
@@ -62,14 +69,17 @@ namespace Status.NewOutages
 
 
                 // As system is a lookup field it has the format <id>;#Text the below fixes that to text
-                SPFieldLookupValue systemlookup = new SPFieldLookupValue(outageSystem);
-                outageSystem = systemlookup.LookupValue;
+                // On moving to multi selectors this has had to change
+                SPFieldLookupValueCollection multichoice = new SPFieldLookupValueCollection(outageSystem);
 
 
-
+               
 
                 
 
+
+                
+                
                 // Loop through recipients here
                 using (StatusDataContext context = new StatusDataContext(prop.Web.Url))
                 {
@@ -77,44 +87,81 @@ namespace Status.NewOutages
                     string usersname;
                     string trackID="x";
 
-                    // Lookup the system for this outage so we can get the 
-                    var systemquery = from systems in context.Systems where systems.Id == systemlookup.LookupId select systems;
-                    foreach (var systems in systemquery) { trackID = systems.TrackID; }
 
-
-                    var userquery = from subscriptions in context.Subscriptions select subscriptions;
-                    foreach (var subscriptions in userquery)
+                    // Loop around each of the systems in turn
+                    foreach (SPFieldLookupValue itemValue in multichoice)
                     {
-                        usersname = "dolbynet\\" + subscriptions.Title;
-                        subscription = subscriptions.SubscribedTo;
-                        if (subscription == null) subscription = ""; // guard against null entries
+                        // Lookup the system for this outage so we can get the trackID
+                        var systemquery = from systems in context.Systems where systems.Id == itemValue.LookupId select systems;
+                        foreach (var systems in systemquery) { trackID = systems.TrackID; }
 
-                        if (subscription.Contains(";" + trackID + ";")) {
-                            // This person needs to be emailed
+                        var userquery = from subscriptions in context.Subscriptions select subscriptions;
+                        foreach (var subscriptions in userquery)
+                        {
+                            usersname = "dolbynet\\" + subscriptions.Title;
+                            subscription = subscriptions.SubscribedTo;
+                            if (subscription == null) subscription = ""; // guard against null entries
 
-                            // Now we start building the email
-                            MailMessage mail = new MailMessage();
-                            mail.From = new MailAddress("rs@dolby.com");
-                            mail.IsBodyHtml = true;
-                            mail.BodyEncoding = System.Text.Encoding.UTF8;
+                            if (subscription.Contains(";" + trackID + ";")) {
+                                // This person needs to be emailed - check to see if they're already a target for this email
+                                if (recipients.IndexOf(subscriptions.Title)== -1) {
+                                    // No existing person found
+                                    recipients.Add(subscriptions.Title);    // Add this person to the recipient list as a target for an email
+                                }
 
-                            mail.Subject = "STATUS ALERT (" + outageSystem + ") " + outageTitle;
-                            mail.Body = "Current Status: " + outageDefcon + Environment.NewLine +
-                                "Update: " + outageUpdate + Environment.NewLine +
-                                "Scope of Impact: " + outageScope + Environment.NewLine +
-                                "User Action Required: " + outageAction + Environment.NewLine +
-                                "Interruption Start Time: " + outageStart + Environment.NewLine +
-                                "Interruption End Time: " + outageEnd + Environment.NewLine +
-                                "Details: " + Environment.NewLine + outageDetails + Environment.NewLine +
-                                "Tracking Ref: " + outageTrackingRef;
-                            SmtpClient smtp = new SmtpClient("mail.dolby.net");
-                            smtp.UseDefaultCredentials = true;
-
-
-                            mail.To.Add(subscriptions.Title + "@dolby.com");
-                            smtp.Send(mail);
+                            }
                         }
                     }
+
+
+                    foreach (string recipient in recipients)
+                    {
+                        int count = 0;
+                        // Now we start building the email
+                        MailMessage mail = new MailMessage();
+                        mail.From = new MailAddress("connect@dolby.com");
+                        mail.IsBodyHtml = true;
+                        mail.BodyEncoding = System.Text.Encoding.UTF8;
+
+                        mail.Subject = "STATUS ALERT - " + outageTitle;
+                        mail.Body += @"<HTML><HEAD><STYLE TYPE=""text/css""> <!-- TD{font-family: Arial; font-size: 10pt;} ---> </STYLE></HEAD><BODY>";
+                        mail.Body += @"<table><tr><td style=""border-bottom=1px solid black; border-right=1px solid black;width: 200px"" valign=""top"">Update</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageUpdate + @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid black;border-right=1px solid black;width: 200px"" valign=""top"">Scope of Impact</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageScope + @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid black;border-right=1px solid black;width: 200px"" valign=""top"">Systems Impacted</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black;width: 600px"">";
+                        foreach (SPFieldLookupValue sysval in multichoice)
+                        {
+                            mail.Body += sysval.LookupValue + @"<BR>";
+                        }
+                        mail.Body += @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid black;border-right=1px solid black;width: 200px"" valign=""top"">Details</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageDetails + @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid black;border-right=1px solid black;width: 200px"" valign=""top"">User Action Required</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageAction + @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid gray;border-right=1px solid black;width: 200px"" valign=""top"">Interruption Start Time</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageStart + @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid gray;border-right=1px solid black;width: 200px"" valign=""top"">Interruption End Time</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageEnd + @"</td></tr>";
+                        mail.Body += @"<tr><td style=""border-bottom=1px solid black;border-right=1px solid black;width: 200px"" valign=""top"">Tracking Reference</td>";
+                        mail.Body += @"<td style=""border-bottom=1px solid black; width: 600px"">" + outageTrackingRef + "</td></tr>";
+
+                        mail.Body += @"</table>";
+                        mail.Body += @"<br><p><font color=""#BBBBBB""><font face=""Arial"">To unsubscribe from notifications related to this system, click <a href=""https://dolbyconnect.dolby.net/LiveStat"">here</a></p>";
+                        mail.Body += @"</BODY></HTML>";
+                        SmtpClient smtp = new SmtpClient("mail.dolby.net");
+                        smtp.UseDefaultCredentials = false;
+
+
+                        mail.To.Add(recipient + "@dolby.net");
+                        smtp.Send(mail);
+                        count++;
+
+                    }
+                    
+                    
+                    
 
                 }
 
@@ -125,4 +172,5 @@ namespace Status.NewOutages
         }
 
     }
+
 }
